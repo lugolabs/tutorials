e3398392cb0781e4e85650f4c2af85da183a3c96
Artan Sinani <artisinani@gmail.com>
2018-05-26 12:38:51 +0200

    Add restricted weeks to jQuery UI weekpicker

--- week-picker/jquery-weekpicker.js
+++ week-picker/jquery-weekpicker.js
@@ -11,6 +11,7 @@
       this._dateFormat = this.options.dateFormat || $.datepicker._defaults.dateFormat;
       var date = this._initialDate();
       this._setWeek(date);
+      this._extendRestrictedWeeks();
       var onSelect = this.options.onSelect;
       this._picker = $(this.element).datepicker($.extend(this.options, this._weekOptions, {
         onSelect: function(dateText, inst) {
@@ -45,22 +46,45 @@
     },
 
     _showDay: function(date) {
+      var dt       = jQuery.datepicker.formatDate(this._dateFormat, date);
+      var show     = this._restrictDates.indexOf(dt) < 0;
       var cssClass = date >= this._startDate && date <= this._endDate ? 'ui-datepicker-current-day' : '';
-      return [true, cssClass];
+      return [show, cssClass];
     },
 
     _setWeek: function(date) {
-      var year = date.getFullYear(),
-        month = date.getMonth(),
-        day   = date.getDate() - date.getDay();
-      this._startDate = new Date(year, month, day);
-      this._endDate   = new Date(year, month, day + 6);
+      var explodedDate = this._explodeDate(date);
+      this._startDate = new Date(explodedDate.year, explodedDate.month, explodedDate.day);
+      this._endDate   = new Date(explodedDate.year, explodedDate.month, explodedDate.day + 6);
     },
 
     _selectCurrentWeek: function() {
       $('.ui-datepicker-calendar')
         .find('.ui-datepicker-current-day a')
         .addClass('ui-state-active');
+    },
+
+    _extendRestrictedWeeks: function() {
+      this._restrictDates = [];
+      if (this.options.restrictWeeks && this.options.restrictWeeks.length) {
+        var date, explodedDate;
+        for (var i = 0; i < this.options.restrictWeeks.length; i++) {
+          date = $.datepicker.parseDate(this._dateFormat, this.options.restrictWeeks[i]);
+          for (var j = 0; j < 7; j++) {
+            explodedDate = this._explodeDate(date);
+            date = new Date(explodedDate.year, explodedDate.month, explodedDate.day + j);
+            this._restrictDates.push(jQuery.datepicker.formatDate(this._dateFormat, date));
+          }
+        }
+      }
+    },
+
+    _explodeDate: function(date) {
+      return {
+        year:  date.getFullYear(),
+        month: date.getMonth(),
+        day:   date.getDate() - date.getDay()
+      };
     }
   });
 
